612357476b8aa1eaf11a40d41b9bfb1e120b5448
kalenlauring <lauringkalen@gmail.com>
2025-12-23 19:57:04 -0500

    initial setup

---
+++ kalenlauringwebsite/src/pages/Homepage.tsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+  return (
+    <div className="min-h-screen bg-blue-600 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Tailwind is working</h1>
+    </div>
+  );
+}
