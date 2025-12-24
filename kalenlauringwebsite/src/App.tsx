1db0bc5588288d3ae3cf6fdbfc3acfd7c605a768
kalenlauring <lauringkalen@gmail.com>
2025-12-24 00:42:01 -0500

    git push

--- kalenlauringwebsite/src/App.tsx
+++ kalenlauringwebsite/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
